06f5193fda688a6ef6fcf63370f9e3274d56ef1a
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-30 07:20:26 +0100

    conditional statements with functions

--- src/components/listGroup.tsx
+++ src/components/listGroup.tsx
@@ -3,10 +3,14 @@ function ListGroup() {
         'New York',
         'Buea',
     ]
+    const getMessage = () => {
+        return items.length === 0 ? <p>No item Found</p> : null;
+}
 
   return (
     <> // react fragment
-      <h1>My list</h1>
+          <h1>My list</h1>
+          {getMessage}
           <ul className="list-group">
               {items.map((item) => <li key={item}>{item}</li>)} its advisable to set a key for identification
          </ul>
