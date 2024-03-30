6aac38d92add614b5df732d18624ba8b2df47c99
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-30 09:33:32 +0100

    created a clickable list and state chage onclick

--- src/components/listGroup.tsx
+++ src/components/listGroup.tsx
@@ -1,19 +1,47 @@
+import { MouseEvent, useState } from "react";
+
 function ListGroup() {
-    const items = [
-        'New York',
-        'Buea',
-    ]
-    const getMessage = () => {
-        return items.length === 0 ? <p>No item Found</p> : null;
-}
+  const items = ["New York", "Buea", "Yaounde", "Douala"];
+  // let selectectedIndex = 0;
+  // items  = [];
+
+  //     const getMessage = () => {
+  //         return items.length === 0 ? <p>No item Found</p> : null;
+  // }
+
+  // Event handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+  // create react hooks: state hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // arr[0] // variable (selected Index)
+  // arr[1] // updater function
 
   return (
-    <> // react fragment
-          <h1>My list</h1>
-          {getMessage}
-          <ul className="list-group">
-              {items.map((item) => <li key={item}>{item}</li>)} its advisable to set a key for identification
-         </ul>
+    <>
+      {/* react fragment*/}
+      <h1>My list</h1>
+      {/* {getMessage} */}
+      {items.length === 0 && <p>No item Found</p>}
+      {/* if true the page will be rendered else the error message will be displayed */}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+        {/*its advisable to set a key for identification*/}
+      </ul>
     </>
   );
 }
