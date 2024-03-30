7e26e79870251eb4f7001296978a20e3f286c040
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-30 07:11:01 +0100

    react frragment and bootstrap

--- src/Messages.tsx
+++ src/Messages.tsx
@@ -1,9 +1,7 @@
+import ListGroup from "./components/listGroup";
+
 function Message() {
-  const name = "Chinepoh";
-  if (name)
-    // return if name is truthy
-    return <h1>Hello {name}!</h1>;
-  else return <h1>Hello World!</h1>;
+  return <div><ListGroup/></div>
 }
 
 export default Message;
